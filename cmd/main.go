b98eaf3e10c183559b8c4adb257cbcb556348f74
steakliu <44339731+steakliu@users.noreply.github.com>
2021-10-31 19:25:43 +0800

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	bc := core.NewBlockChain()
 	bc.SendData("can I help you")
-	bc.SendData("can I help you")
+	//bc.SendData("can I help you")
 	bc.SendData("can I help you")
 	bc.Print()
 }
